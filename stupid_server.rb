cede9652a89a3509506b8b65713406767f9bdbf9
Thomas Lecavelier <ook@ookook.fr>
2019-02-11 13:16:33 +0100

    Stupid HTTP with info on local and remote extremities

--- stupid_server.rb
+++ stupid_server.rb
@@ -1,15 +1,32 @@
 require 'socket'
+require 'cgi'
 server = TCPServer.new 5678
 
 while session = server.accept
   request = session.gets
   puts request
 
-  session.print "HTTP/1.1 200\r\n"
-  session.print "Content-Type: text/html\r\n"
-  session.print "\r\n"
-  session.print "Hello world! The time is #{Time.now}"
-  session.print "\r\n"
+  response = <<~HTML
+    HTTP/1.1 200
+    Content-Type: text/html
+
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+          <meta charset="utf-8">
+          <title>Stupid server said</title>
+      </head>
+      <body>
+         <p>Hello world! Here the time: #{Time.new}</p>
+         <p>#{CGI.escapeHTML(session.peeraddr.inspect)}</p>
+         <p>#{CGI.escapeHTML(session.addr.inspect)}</p>
+         <p>#{CGI.escapeHTML(session.remote_address.inspect)}</p>
+         <p>#{CGI.escapeHTML(session.local_address.inspect)}</p>
+      </body>
+    </html>
+
+  HTML
+  session.print response
 
   session.close
 end
